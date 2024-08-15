b6015b847b784abb79ffc980d6775ad45a208f12
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2024-08-15 08:58:21 -0600

    new transaction into create diagnostic server

--- src/services/diagnostic/createDiagnostic.ts
+++ src/services/diagnostic/createDiagnostic.ts
@@ -1,67 +1,66 @@
 import { DiagnosticPropierties, DiagnosticProps } from 'interfaces'
 import { getVehiculeById } from '../vehicule/getVehicule'
-import { createUnitStatus } from './createUnitStatus'
 import { CreateDiagnosticError } from 'errors'
-import { createPreviousCheck } from './createPreviousCheck'
-import { createCheckDone } from './createChecksDone'
 import { createRecommendation } from './createRecommendations'
-import { DiagnosticModel } from 'models/diagnostic'
+import {
+  ActivityTypeDiagnosticModel,
+  ChecksDoneDiagnosticModel,
+  DiagnosticModel,
+  PossibleFailuresDiagnosticModel,
+  PreviousCheckDiagnosticModel,
+  UnitStatusModel
+} from 'models/diagnostic'
 import { getClientByIdService } from 'services/client/getClient'
-import { createPossibleFailure } from './createPossibleFailures'
-import { createActivityTypeService } from './createActivityType'
+import mongoose from 'mongoose'
 
 export const createDiagnoticService = async (diagnostic: DiagnosticProps): Promise<DiagnosticPropierties> => {
-  const vehicule = await getVehiculeById(diagnostic.vehiculeId)
-  if (!vehicule) {
-    throw new CreateDiagnosticError('Vehicule not found')
+  const session = await mongoose.startSession()
+  session.startTransaction()
+
+  try {
+    const vehicule = await getVehiculeById(diagnostic.vehiculeId)
+    if (!vehicule) {
+      throw new CreateDiagnosticError('Vehicule not found')
+    }
+
+    const client = await getClientByIdService(diagnostic.clientId)
+    if (!client) {
+      throw new CreateDiagnosticError('Client not found')
+    }
+
+    const unitStatus = new UnitStatusModel(diagnostic.unitStatus)
+    const previousCheck = new PreviousCheckDiagnosticModel(diagnostic.previousCheck)
+    const checksDone = new ChecksDoneDiagnosticModel(diagnostic.checksDone)
+    const possibleFailures = new PossibleFailuresDiagnosticModel(diagnostic.possibleFailures)
+    const activityType = new ActivityTypeDiagnosticModel(diagnostic.activityType)
+    const recommendations = await createRecommendation(diagnostic.recommendations)
+    if (!recommendations) {
+      throw new CreateDiagnosticError('Recommendations not registered')
+    }
+
+    const newDiagnostic = new DiagnosticModel({
+      checksDone,
+      previousCheck,
+      recommendations,
+      unitStatus,
+      possibleFailures,
+      activityType,
+      vehicule,
+      client,
+    })
+
+    unitStatus.save({ session })
+    previousCheck.save({ session })
+    checksDone.save({ session })
+    possibleFailures.save({ session })
+    newDiagnostic.save({ session })
+
+    return newDiagnostic
+  } catch (error) {
+    await session.abortTransaction()
+    throw new CreateDiagnosticError(String(error))
+  } finally {
+    session.endSession()
   }
-
-  const client = await getClientByIdService(diagnostic.clientId)
-  if (!client) {
-    throw new CreateDiagnosticError('Client not found')
-  }
-
-  const unitStatus = await createUnitStatus(diagnostic.unitStatus)
-  if (!unitStatus) {
-    throw new CreateDiagnosticError('Unit status not registered')
-  }
-
-  const previousCheck = await createPreviousCheck(diagnostic.previousCheck)
-  if (!previousCheck) {
-    throw new CreateDiagnosticError('Previous check not registered')
-  }
-
-  const checksDone = await createCheckDone(diagnostic.checksDone)
-  if (!checksDone) {
-    throw new CreateDiagnosticError('Check done not registered')
-  }
-
-  const possibleFailures = await createPossibleFailure(diagnostic.possibleFailures)
-  if (!possibleFailures) {
-    throw new CreateDiagnosticError('possible failures not registered')
-  }
-
-  const activityType = await createActivityTypeService(diagnostic.activityType)
-  if (!activityType) {
-    throw new CreateDiagnosticError('activity types not registered')
-  }
-
-  const recommendations = await createRecommendation(diagnostic.recommendations)
-  if (!recommendations) {
-    throw new CreateDiagnosticError('Recommendations not registered')
-  }
-
-  const newDiagnostic = await DiagnosticModel.create({
-    checksDone,
-    previousCheck,
-    recommendations,
-    unitStatus,
-    possibleFailures,
-    activityType,
-    vehicule,
-    client,
-  })
-
-  return newDiagnostic
 }
 
