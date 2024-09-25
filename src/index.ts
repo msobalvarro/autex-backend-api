31ccd712ee0a50562e8bc9c5d125c218be04d747
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2024-09-24 18:02:16 -0600

    some configuration

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
 import { router } from './routes'
 import { dbConnection } from './config/mongo'
 
 const PORT = process.env.PORT
 const app = express()
 dbConnection().then(() => {
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  })
+
   app.use(cors())
   app.use(express.json())
   app.use(router)
