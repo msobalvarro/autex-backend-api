ae91b434ecf10660284154c054e9f1fa796daf0e
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2024-09-25 15:31:42 -0600

    tesgt

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,6 +5,8 @@ import { ReqHeaderAuthPropierties } from 'interfaces'
 
 const SECRET_KEY: string = process.env.SECRET_KEY || ''
 
+console.log('secret key:', SECRET_KEY)
+
 export const createHash = (data: string): string => crypto.createHash('sha256').update(data).digest('hex')
 
 export const verifyToken = (jwt: string) => verify(jwt, SECRET_KEY)
