5d791cf814123ad4620e53e6a18725ac2b88512a
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2024-09-25 16:10:52 -0600

    update script

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,10 +3,10 @@ import crypto from 'crypto'
 import { sign, verify } from 'jsonwebtoken'
 import { ReqHeaderAuthPropierties } from 'interfaces'
 
-// const SECRET_KEY: string = process.env.SECRET_KEY || ''
+const SECRET_KEY: string = process.env.SECRET_KEY || ''
 
 export const createHash = (data: string): string => crypto.createHash('sha256').update(data).digest('hex')
 
-export const verifyToken = (jwt: string) => verify(jwt, `${process.env.SECRET_KEY}`)
+export const verifyToken = (jwt: string) => verify(jwt, SECRET_KEY)
 
-export const generateToken = (props: ReqHeaderAuthPropierties) => sign(props, `${process.env.SECRET_KEY}`, { expiresIn: '24h' })
+export const generateToken = (props: ReqHeaderAuthPropierties) => sign(props, SECRET_KEY, { expiresIn: '24h' })
