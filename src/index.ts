503213807b128059477b63a60f683373194a1112
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2024-09-30 10:45:53 -0600

    bug fixed routing

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,15 @@ const app = express()
 dbConnection().then(() => {
   app.use(express.static(path.join(__dirname, '../dist')))
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'))
   })
 
   app.use(cors())
   app.use(express.json())
   app.use(router)
+  app.get('*', (__, res) => {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  })
   // app.use()
   app.listen(PORT, () => console.log(`ready into port ${PORT}`))
 }).catch(err => console.log(`database connection error: ${err}`))
